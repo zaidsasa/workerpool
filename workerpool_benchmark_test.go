817583591121143be5b4263a2f1f81eb5cc56fdf
Zaid Sasa <zaidsasa.tech@gmail.com>
2024-02-23 16:51:36 +0100

    update benchmark

--- workerpool_benchmark_test.go
+++ workerpool_benchmark_test.go
@@ -2,17 +2,38 @@ package workerpool_test
 
 import (
 	"context"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/zaidsasa/workerpool"
 )
 
 const (
-	poolSize = 10
-	runCount = 1000
+	poolSize      = 5e4
+	runCount      = 1e6
+	sleepDuration = 10 * time.Millisecond
 )
 
-func stubFunc() {}
+func stubFunc() {
+	time.Sleep(sleepDuration)
+}
+
+func BenchmarkGoroutines(b *testing.B) {
+	var waitGroup sync.WaitGroup
+
+	for i := 0; i < b.N; i++ {
+		waitGroup.Add(runCount)
+
+		for j := 0; j < runCount; j++ {
+			go func() {
+				stubFunc()
+				waitGroup.Done()
+			}()
+		}
+		waitGroup.Wait()
+	}
+}
 
 func BenchmarkWorkerPool(b *testing.B) {
 	wPool, _ := workerpool.New(poolSize)
